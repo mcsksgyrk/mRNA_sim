f69319502dd1acc2d6668a8666e43ce8acfeb041
mcsksgyrk <bbence.hajdu@gmail.com>
2024-10-15 13:47:06 +0200

    csv

--- src/mrnamodels.jl
+++ src/mrnamodels.jl
@@ -1,5 +1,5 @@
-include("submodules/model.jl")
-include("submodules/parsers.jl")
+include("src/submodules/model.jl")
+include("src/submodules/parsers.jl")
 
 using .model
 using .parsers
@@ -17,12 +17,11 @@ function make_df_row(sol, patient)
 end
 
 # Parse patient data
-data_df = DataFrame(XLSX.readtable("../source/patient_level_mirna_20240514.xlsx",
+data_df = DataFrame(XLSX.readtable("./source/patient_level_mirna_20240514.xlsx",
                                    "patient_configurations"))
 sort!(data_df, :Patient)
 patients = sort(unique(values(data_df.Patient)))
 rnaParams = rnaParamValues()
-
 param_sets = Dict{String, rnaParamValues}()
 for p in patients
     df = data_df[(data_df.Patient.==p), :]
@@ -44,15 +43,66 @@ u0 =[0.01601;
     0.32258;
     0.32258]
 tspan = (0.0,100.0)
-stress_free_results = DataFrame([[],[],[],[],[]], ["patient", "BEC1_min", "BEC1_max", "NFKB_min", "NFKB_max"])
+# basal state
+basal_results = DataFrame([[],[],[],[],[]], ["patient", "NFKB_min", "NFKB_max", "BEC1_min", "BEC1_max"])
 for (key, value) in param_sets
     p = value
     prob = ODEProblem(miRNA_model, u0, tspan, p)
     sol = solve(prob, Rosenbrock23())
-    push!(stress_free_results, make_df_row(sol,key))
+    push!(basal_results, make_df_row(sol,key))
 end
-stress_free_results
-plot(sol.t,sol[1,:], ylim=(0, 1), label="BEC1")
-plot!(sol.t,sol[2,:], ylim=(0, 1), label="NFKB")
-sol.t
-
+basal_results
+############
+stress_results = DataFrame([[],[],[],[],[]], ["patient", "NFKB_min", "NFKB_max", "BEC1_min", "BEC1_max"])
+for (key, value) in param_sets
+    p = value
+    prob = ODEProblem(miRNA_model_stress, u0, tspan, p)
+    sol = solve(prob, Rosenbrock23())
+    push!(stress_results, make_df_row(sol,key))
+end
+maximum.(eachcol(stress_results))
+minimum.(eachcol(stress_results))
+maximum.(eachcol(basal_results))
+#plot(sol.t,sol[1,:], ylim=(0, 1), label="NFKB")
+#plot!(sol.t,sol[2,:], ylim=(0, 1), label="BEC1")
+#sol.t
+#stress_results[stress_results.patient="10252"]
+## Extract the row labels (keys)
+#rows = collect(keys(param_sets))  # ["Row1", "Row2", "Row3"]
+#rows
+## Dynamically extract all 19 fields from the struct into a matrix
+#num_fields = fieldcount(rnaParamValues)  # Number of fields in the struct (19)
+#field_names = fieldnames(rnaParamValues)
+#data_matrix = [getfield(v, field) for v in values(param_sets), field in field_names]
+## Plot heatmap
+#heatmap(data_matrix,
+#        xlabel="Fields",
+#        ylabel="Rows",
+#        xticks=(1:length(field_names), field_names),  # Dynamically label fields
+#        yticks=(1:length(rows), rows),
+#        color=:viridis,
+#        title="Heatmap from Custom Structs with Unique Fields",
+#        ticksrotation=90,
+#        xrotation=90.0)
+#
+#fields_to_plot = [:card9_mir486, :gpr18_mir486]
+#field_indices = findall(field -> field in fields_to_plot, field_names)
+#heatmap(data_matrix,
+#        xlabel="Fields",
+#        ylabel="Patients",
+#        xticks=(1:length(fields_to_plot), fields_to_plot),  # Label only the specified fields
+#        yticks=(1:length(rows), rows),
+#        color=:viridis,
+#        title="Heatmap for Selected Fields",
+#        xrotation=90)  # Rotate x-axis labels by 90 degrees
+#using Clustering
+#data_matrix
+## Convert to a matrix
+#data_matrix = hcat(data_matrix...)  # Transpose for correct orientation
+#d = data_matrix+data_matrix'
+## Perform hierarchical clustering
+#dendrogram = hclust(data_matrix)  # Use transpose for correct dimensions
+#
+## Plot the dendrogram
+#dendrogram_plot = dendrogram_plot(dendrogram, labels=keys(data_dict), title="Hierarchical Clustering Dendrogram")
+#display(dendrogram_plot)
--- src/submodules/model.jl
+++ src/submodules/model.jl
@@ -2,8 +2,82 @@ module model
 
 using Parameters
 
-export miRNA_model, rnaParamValues
+export miRNA_model, miRNA_model_stress
 
+function miRNA_model_stress(du, u, p, t)
+    mir339, mir486, mir6803, mir128, mir92, mir885, mir146a, mir125, mir504, mir146b = ones(10)
+    NFKBT, BEC1T, CARD9T, GPR35T, INAVAT, GPR18T, APEHT, GPR65T, PARK7T = ones(10)
+
+    # kanf=1.5 kanf1=1, kanf2=1, kabc=0.1, kabc1=2, kabc2=0.1
+    #stress faktor
+    kanf=1.5
+    kanf1=1.0
+    kanf2=1.0
+    #
+    kinf=1
+    #kinf1=1
+    kinf1=0.5
+    kinf2=15
+    Jnfk=0.1
+    #stress faktor
+    kabc=0.1
+    kabc1=2.0
+    kabc2=0.1
+    #
+    kibc=0.5
+    kibc1=1
+    kibc2=15
+    Jbec=0.1
+    kacd9=1
+    kicd9=0.1
+    kagr35=1
+    kigr35=0.1
+    kaiaa=1
+    kiiaa=0.1
+    kaaph=1
+    kiaph=0.1
+    kagr18=1
+    kigr18=0.1
+    kagr65=1
+    kigr65=0.1
+    kapk7=1
+    kipk7=0.1
+
+    GPR1 = u[3]
+
+    GPR2 = u[4] + u[5] + u[6]
+
+    GPR3 = u[7] + u[8] + u[9]
+
+    du[1] = (kanf + kanf1*GPR1 + kanf2*GPR2)*(NFKBT-u[1])/(Jnfk + NFKBT-u[1]) -
+                (kinf + kinf1*GPR3 + kinf2*u[2])*u[1]/(Jnfk + u[1])
+
+    du[2] = (kabc + kabc1*GPR2 + kabc2*GPR3)*(BEC1T-u[2])/(Jbec + BEC1T-u[2]) -
+                (kibc + kibc1*GPR1 + kibc2*u[1])*u[2]/(Jbec + u[2])
+
+    du[3] = kacd9*(CARD9T - u[3]) -
+                (kicd9 + p.card9_mir6803*mir6803 + p.card9_mir486*mir486)*u[3]
+
+    du[4] = kagr35*(GPR35T - u[4]) -
+                (kigr35 + p.gpr35_mir6803*mir6803 + p.gpr35_mir128*mir128 +
+                    p.gpr35_mir92*mir92 + p.gpr35_mir125*mir125)*u[4]
+
+    du[5] = kaiaa*(INAVAT - u[5]) -
+                (kiiaa + p.inava_mir504*mir504 + p.inava_mir146b*mir146b +
+                p.inava_mir125*mir125)*u[5]
+
+    du[6] = kaaph*(APEHT - u[6]) - (kiaph + p.apeh_mir504*mir504)*u[6]
+
+    du[7] = kagr18*(GPR18T - u[7]) -
+                (kigr18 + p.gpr18_mir885*mir885 + p.gpr18_mir146a*mir146a + p.gpr18_mir92*mir92 +
+                p.gpr18_mir125*mir125 + p.gpr18_mir504*mir504 + p.gpr18_mir486*mir486)*u[7]
+
+    du[8] = kagr65*(GPR65T - u[8]) -
+                (kagr65 + kigr65 + p.gpr65_mir128*mir128)*u[8]
+
+    du[9] = kapk7*(PARK7T - u[9]) -
+                (kipk7 + p.park7_mir486*mir486 + p.park7_mir339*mir339)*u[9]
+end
 function miRNA_model(du, u, p, t)
     mir339, mir486, mir6803, mir128, mir92, mir885, mir146a, mir125, mir504, mir146b = ones(10)
     NFKBT, BEC1T, CARD9T, GPR35T, INAVAT, GPR18T, APEHT, GPR65T, PARK7T = ones(10)
@@ -18,7 +92,8 @@ function miRNA_model(du, u, p, t)
     #kanf2=1.0
     #
     kinf=1
-    kinf1=1
+    #kinf1=1
+    kinf1=0.5
     kinf2=15
     Jnfk=0.1
     #stress faktor
